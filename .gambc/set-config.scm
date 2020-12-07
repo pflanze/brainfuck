0385179ffbf29f0b306bae5f74bad839598a22fd
Christian Jaeger <ch@christianjaeger.ch>
2020-12-07 13:36:38 +0000

    Update loader config

--- .gambc/set-config.scm
+++ .gambc/set-config.scm
@@ -1,2 +1,18 @@
 (define compile-mode 'c) ;; s source, c compile, (i is currently buggy)
-(define compile-options '(options: (debug keep-c) cc-options: "-g"))
+
+(define set-config:system-type
+  ;; (let* ((p (open-input-process (list path: "bin/system-type")))
+  ;;        (out (read-line p)))
+  ;;   (if (zero? (process-status p))
+  ;;       out
+  ;;       (error "subprocess gave error")))
+  "normal")
+
+(define compile-options
+  (if (string=? set-config:system-type "RPI")
+      '(
+        options: (keep-c))
+      '(
+        options: (debug keep-c)
+        cc-options: "-g")))
+
